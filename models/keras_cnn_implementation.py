bd1c8c60241d10e9a974b669eb22c583c3b09723
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2023-08-14 22:02:05 +0200

    add pooling layer

--- models/keras_cnn_implementation.py
+++ models/keras_cnn_implementation.py
@@ -1,19 +1,24 @@
-from keras import Sequential
-from keras.layers import Dense, Activation, Bidirectional, LSTM, CuDNNLSTM, Embedding
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers import Embedding
+from keras.preprocessing import sequence
 
 ## hyper parameters
 batch_size = 32
 embedding_dims = 1560 # Length of the token vectors
 filters = 250 # number of filters in your Convnet
-kernel_size = 6 # a window size of 3 tokens
 hidden_dims = 250 #number of neurons at the normal feedforward NN
 epochs = 2
 
 model = Sequential()
-model.add(Embedding(
-  input_dim=44, 
-  output_dim=3, 
-  input_length=max_len))
+model.add(Conv1D())
+model.add(MaxPooling1D())
+model.add(Flatten())
+model.add(Dense(250, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
