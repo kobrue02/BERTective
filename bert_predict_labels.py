cea4e8e7e2383ec45b193bda421c236e766ecc46
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2023-08-17 22:24:14 +0200

    all label support

--- bert_predict_labels.py
+++ bert_predict_labels.py
@@ -10,6 +10,8 @@ def jsonline_to_row(label: str, prodigy_item: dict) -> str:
     return_row = ""
     label_span = 0
 
+    if 'spans' not in list(prodigy_item.keys()):
+        return str("O " * len(prodigy_item['tokens']))[:-1]
     if len(prodigy_item['spans']) == 0:
         return str("O " * len(prodigy_item['tokens']))[:-1]
     
@@ -26,23 +28,17 @@ def jsonline_to_row(label: str, prodigy_item: dict) -> str:
     return return_row[:-1]
 
 path = "data/annotation/sentence_level_annotations.jsonl"
+all_labels = ['wOrder', 'wFehlt', 'Interpkt.', 'Rechtschr.', 'ugs.', 'Angliz.', 'geh.', 'emo.', 'wRedund', 'Jarg.']
+label_item_lists = {}
+
 with jsonlines.open(path, mode='r') as reader:
     objects = [item for item in reader]
 
-for object in objects:
-    try:
-        if len(object["spans"]) > 1:
-            pprint(object)
-            print('-' * 64)
-            print(jsonline_to_row('Interpkt.', object))
-    except KeyError:
-        pprint(object)
-        continue
-
-
-
-
-
-training_dataset = pd.DataFrame()
+for label in all_labels:
+    label_item_lists[label] = [jsonline_to_row(label, obj) for obj in objects]
+label_item_lists['text'] = [obj['text'] for obj in objects]
+    
+training_dataset = pd.DataFrame(label_item_lists)
+print(training_dataset.head())
 
 
