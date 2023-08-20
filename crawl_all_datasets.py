c5e4bcd11c29507f0b4833a323ebaf19f568a888
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2023-08-20 15:46:11 +0200

    small fix

--- crawl_all_datasets.py
+++ crawl_all_datasets.py
@@ -2,5 +2,7 @@ import scraping_tools.korrekturen_scrape as ortho_data
 import scraping_tools.achse_des_guten_api as achse_data
 
 def download_data(sources: list = ['ortho', 'achse', 'zdl', 'gutenberg', 'reddit']):
-    ortho_data.run()
-    achse_data.run()
+    if 'ortho' in sources:
+        ortho_data.run()
+    if 'achse' in sources:
+        achse_data.run()
