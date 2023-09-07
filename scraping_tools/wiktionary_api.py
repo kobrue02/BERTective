216ebf6957d454797731063d3920880dae80980d
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2023-09-07 23:21:03 +0200

    turned script into functions

--- scraping_tools/wiktionary_api.py
+++ scraping_tools/wiktionary_api.py
@@ -5,6 +5,48 @@ import json
 from bs4 import BeautifulSoup
 from pprint import pprint
 
+WIKI_LIST = [
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Abkürzungen_im_Internet',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Anglizismen',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Arabismen',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Aufwertung',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Binomiale',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Biologie/Fachwortliste',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Chemie/Fachwortliste',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Chinesische_Wörter_in_der_deutschen_Sprache',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/DDR-Sprache',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Gallizismen',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Geowissenschaften',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Geschichte',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Hispanismen',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Japanische_Wörter_in_der_deutschen_Sprache',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Jägersprache',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Mathematik/Fachwortliste',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Medizin/Fachwortliste',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Militär/Fachwortliste',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Musik/Fachwortliste',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Netzjargon',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Ornithologie/Fachwortliste',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Pharmazie',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Philosophie',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Physik/Fachwortliste',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Politik',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Rechtswissenschaften',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Religion',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Romanismen',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Slawismen',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Weinbau',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Wirtschaft',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Griechische_Präfixe',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Griechische_Suffixe',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Informationstechnik',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Lateinische_Präfixe',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Lateinische_Suffixe',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Linguistik/Fachwortliste',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Philatelie',
+        'https://de.download_wiktionary.org/wiki/Verzeichnis:Deutsch/Sprichwörter'
+    ]
+
 
 def call_url(url: str):
     r = requests.get(url)
@@ -45,65 +87,26 @@ def get_words_word_list(L: list) -> list:
         except IndexError:
             break
     return target
-if __name__ == "__main__":
-    wiki_list = [
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Abkürzungen_im_Internet',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Anglizismen',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Arabismen',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Aufwertung',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Binomiale',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Biologie/Fachwortliste',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Chemie/Fachwortliste',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Chinesische_Wörter_in_der_deutschen_Sprache',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/DDR-Sprache',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Gallizismen',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Geowissenschaften',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Geschichte',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Hispanismen',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Japanische_Wörter_in_der_deutschen_Sprache',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Jägersprache',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Mathematik/Fachwortliste',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Medizin/Fachwortliste',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Militär/Fachwortliste',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Musik/Fachwortliste',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Netzjargon',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Ornithologie/Fachwortliste',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Pharmazie',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Philosophie',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Physik/Fachwortliste',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Politik',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Rechtswissenschaften',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Religion',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Romanismen',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Slawismen',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Weinbau',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Wirtschaft',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Griechische_Präfixe',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Griechische_Suffixe',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Informationstechnik',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Lateinische_Präfixe',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Lateinische_Suffixe',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Linguistik/Fachwortliste',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Philatelie',
-        'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Sprichwörter'
-    ]
-    #L = find_word_list(f'https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Anglizismen')
 
-    wiktionary = {}
+def __wiktionary(wiki_list: list[str]):
+    download_wiktionary = {}
     for url in wiki_list:
         base = str(url.split('/')[-1])
         print(url)
         L = find_word_list(url)
         target = get_words_word_list(L)
         if len(target) > 1:
-            wiktionary[base] = target
+            download_wiktionary[base] = target
         else:
             L = find_word_list_other(url)
             target = [w.get_text() for w in L]
             if len(target) > 1:
-                wiktionary[base] = target
+                download_wiktionary[base] = target
+    with open('data/download_wiktionary/download_wiktionary.json', 'w', encoding='utf-8') as f:
+        json.dump(download_wiktionary, f) 
 
-pprint(wiktionary)
-print(list(wiktionary.keys()))
-with open('data/wiktionary/wiktionary.json', 'w', encoding='utf-8') as f:
-    json.dump(wiktionary, f) 
+def download_wiktionary():
+    __wiktionary(WIKI_LIST)
+
+if __name__ == "__main__":
+    download_wiktionary()
