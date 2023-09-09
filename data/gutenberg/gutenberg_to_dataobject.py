105ef94b51f97a3e8cb7cb34b9b13cc3f6141e1c
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2023-09-09 08:45:36 +0200

    add script to align gutenberg data and turn into dataobjects

---
+++ data/gutenberg/gutenberg_to_dataobject.py
@@ -0,0 +1,26 @@
+import json
+from pprint import pprint
+
+
+def _align_dicts(data: dict, author_dict: dict) -> dict:
+    full_dict = {"books": []}
+    for data_item in data['texts']:
+        try:
+            y = next(item for item in author_dict['books'] if item["book_name"] == data_item['book_name'])
+        except StopIteration:
+            break
+        temp = dict(y)
+        temp.update({'text': data_item["text"]})
+        full_dict['books'].append(temp)
+    return full_dict
+
+if __name__ == "__main__":
+
+    with open('data/gutenberg/data.json', 'r') as f:
+        data = json.load(f)
+
+    with open('data/gutenberg/author_dict.json', 'r') as f:
+        author_dict = json.load(f)
+
+    authors = _align_dicts(data, author_dict)
+    pprint(authors)
